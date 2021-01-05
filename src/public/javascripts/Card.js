705a6f1ba8fbb38673dc26ec3e507c853c25420b
Raymond shi <rs5955@nyu.edu>
2021-01-05 16:05:06 -0500

    Fix format

--- src/public/javascripts/Card.js
+++ src/public/javascripts/Card.js
@@ -1,10 +1,10 @@
 class Card {
-    constructor(rank, suit) {
-      this.rank = rank; //2-10, J, Q, K, A
-      this.suit = suit; //0-3 (0:heart,1:diamond,2:spade,3:club)
-    }
-    //to check for card equality
-    equals(card2) {
-      return this.rank === card2.rank && this.suit === card2.suit;
-    }
+  constructor(rank, suit) {
+    this.rank = rank; //2-10, J, Q, K, A
+    this.suit = suit; //0-3 (0:heart,1:diamond,2:spade,3:club)
   }
+  //to check for card equality
+  equals(card2) {
+    return this.rank === card2.rank && this.suit === card2.suit;
+  }
+}
